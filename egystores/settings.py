124f086d637bacdcc739039e259a1991db6302c9
Abdallah Eslah <abdallaheslah97@gmail.com>
2020-11-04 20:52:57 +0200

    New Commit Change

--- egystores/settings.py
+++ egystores/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,8 +129,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/restaurant/login/'
